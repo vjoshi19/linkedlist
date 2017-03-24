fb07ba26d35a4f0b88454e9a1e73c2ce83d2b738
Varsha Joshi <vjoshi19@outlook.com>
2017-03-24 12:42:16 -0700

    moreImplemenations of ll

--- LinkedList1.java
+++ LinkedList1.java
@@ -4,6 +4,62 @@ public class LinkedList1 {
 	Node1 head;
 	//Node1 tail;
 	
+	
+	int CompareLists(Node1 headA, Node1 headB) {
+	    if (headA == null && headB == null) {
+	        return 1;
+	    }
+	    if (headA == null || headB == null || headA.data != headB.data) {
+	        return 0;
+	    }
+	    return CompareLists(headA.next, headB.next);
+	}
+	
+	Node1 Delete(Node1 head, int position){
+		// Empty list 
+		if(head == null){
+			return null;
+		}
+		// List with just 1 element
+		if(head.next == null && position == 0){
+			//System.out.println("Head.next->null");
+			head = null;
+			return head;
+		}
+		
+		if(head.next != null && position == 0){
+			head = head.next;
+			return head;
+		}
+		// Last item on the list
+		
+		// List with more than 1 element
+		Node1 temp = head;
+		Node1 previous = head;
+		//System.out.println("Previous: " + previous.data);
+		int index = 0;
+		while(index != position){
+			if(index >= 1){
+				previous = temp;
+				//System.out.println("Previous: " + previous.data);
+			}
+			
+			temp = temp.next;
+			//System.out.println("Data: " + temp.data);
+			++index;
+		}
+		//System.out.println("Index: " + index + " position: " + position);
+		
+		if(temp.next == null){
+			//System.out.println("temp.next is null");
+			previous.next = null;
+		}else{
+			previous.next = temp.next;
+		}
+		
+		temp = null;
+		return head;	
+	}
 	Node1 InsertNth(Node1 head, int data, int position){
 		//System.out.println("func: " + head.data);
 		int index = 0;
@@ -11,10 +67,12 @@ public class LinkedList1 {
 		n.data = data;
 		
 		if(head == null){
+			System.out.println("Head was null");
 			return n;
 		}
 		if(position == 0 ){
-			n.next = head;
+			System.out.println("Inserting 0th position");
+			//n.next = head;
 			head = n;
 			//head.data = n.data;
 			//head.next = null;
@@ -33,8 +91,6 @@ public class LinkedList1 {
 			temp.next = n;
 			return head;
 		}
-		
-		
 	}
 	public void printList(Node1 head){
 		Node1 current = head;
@@ -44,5 +100,30 @@ public class LinkedList1 {
 			current = current.next;
 		}
 		System.out.println("null");
+	}
+	
+	public void printReverse(Node1 head){
+		if (head == null){
+			return;
+		}
+		printReverse(head.next);
+		System.out.println(head.data);
+	}
+	
+	public Node1 addAtStart (int data){
+		// Scenario 1: Empty linked list
+		Node1 newnode = new Node1();
+		newnode.data = data;
+		
+		if(head == null){
+			head = newnode;
+			System.out.println("Added new Node: " + head.data);
+		}else{
+			// Scenario 2: Non empty linked list: add Node(10) to [Node(13), Node(7)]
+			newnode.next = head;
+			head = newnode;
+			System.out.println("Added new Node: " + head.data);
+		}
+		return head;
 	}
 }
--- TestLL1.java
+++ TestLL1.java
@@ -4,11 +4,26 @@ public class TestLL1 {
 
 	public static void main(String[] args) {
 		LinkedList1 ll1 = new LinkedList1();
-		Node1 head = new Node1();
-		Node1 newHead = ll1.InsertNth(head, 3, 0);
-		ll1.printList(newHead);
+		LinkedList1 ll2 = new LinkedList1();
+		Node1 head1 = new Node1();
+		Node1 head2 = new Node1();
+		
+		head1 = ll1.addAtStart(7);
+		head1 = ll1.addAtStart(9);
+		ll1.printList(head1);
+		head2 = ll2.addAtStart(7);
+		ll2.printList(head2);
+		System.out.println(ll1.CompareLists(head1, head2));
+		//System.out.println("Deleting null head");
+		//newHead = ll1.Delete(head, 0);
+		//System.out.println("data:" + deleted.data);
+		//System.out.println("data:" + deleted.next);
+		/*ll1.printList(newHead);
 		
 		newHead = ll1.InsertNth(newHead, 5, 1);
+		ll1.printList(newHead);
+		System.out.println("Deleting item at pos 1 ");
+		newHead = ll1.Delete(newHead, 1);
 		ll1.printList(newHead);
 		newHead = ll1.InsertNth(newHead, 4, 2);
 		ll1.printList(newHead);
@@ -16,6 +31,14 @@ public class TestLL1 {
 		ll1.printList(newHead);
 		newHead = ll1.InsertNth(newHead, 10, 1);
 		ll1.printList(newHead);
+		
+		ll1.printReverse(newHead);*/
+		/*System.out.println("Deleting item at pos 3 ");
+		newHead = ll1.Delete(newHead, 3);
+		ll1.printList(newHead);
+		System.out.println("Deleting item at pos 0 ");
+		newHead = ll1.Delete(newHead, 0);
+		ll1.printList(newHead);*/
 	}
 
 }
